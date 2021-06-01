37cc6ae43391ae0d2f804c5fc53916d53341bb15
akos <to.easyweb@gmail.com>
2021-06-01 18:13:29 +0300

    Update medium8-1-2.cs

--- medium8-1-2.cs
+++ medium8-1-2.cs
@@ -1,89 +1,194 @@
 using System;
-using System.Collections.Generic;
 
 namespace Task
 {
-    class Program
+  class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            int obj1x = 5;
-            int obj1y = 5;
-            bool isalive1 = true;
-            int obj2x = 10;
-            int obj2y = 10;
-            bool isalive2 = true;
-            int obj3x = 15;
-            int obj3y = 15;
-            bool isalive3 = true;
+      Object[] objects =
+      {
+        new Object(new Position(5, 5)),
+        new Object(new Position(10, 10)),
+        new Object(new Position(15, 15))
+      };
 
-            Random random = new Random();
+      bool[] isObjectAlive = new bool[objects.Length];
+
+      while (true)
+      {
+        Update(objects, isObjectAlive);
+      }
+    }
+
+    private static void Update(Object[] objects, bool[] isObjectAlive)
+    {
+      for (int i = 0; i < objects.Length; i++)
+      {
+        if (i < objects.Length - 1)
+        {
+            Object[] objects = 
+            { 
+                new Object(new Position(5, 5)), 
+                new Object(new Position(10, 10)), 
+                new Object(new Position(15, 15)) 
+            };
+            
+            Renderer renderer = new Renderer(objects);
 
             while (true)
             {
-                if (obj1x == obj2x && obj1y == obj2y)
-                {
-                    isalive1 = false;
-                    isalive2 = false;
-                }
-
-                if (obj1x == obj3x && obj1y == obj3y)
-                {
-                    isalive1 = false;
-                    isalive3 = false;
-                }
-
-                if (obj2x == obj3x && obj2y == obj3y)
-                {
-                    isalive2 = false;
-                    isalive3 = false;
-                }
-
-                obj1x += random.Next(-1, 1);
-                obj1y += random.Next(-1, 1);
-
-                obj2x += random.Next(-1, 1);
-                obj2y += random.Next(-1, 1);
-
-                obj3x += random.Next(-1, 1);
-                obj3y += random.Next(-1, 1);
-
-                if (obj1x < 0)
-                    obj1x = 0;
-
-                if (obj1y < 0)
-                    obj1y = 0;
-
-                if (obj2x < 0)
-                    obj2x = 0;
-
-                if (obj2y < 0)
-                    obj2y = 0;
-
-                if (obj3x < 0)
-                    obj3x = 0;
-
-                if (obj3y < 0)
-                    obj3y = 0;
-
-                if (isalive1)
-                {
-                    Console.SetCursorPosition(obj1x, obj1y);
-                    Console.Write("1");
-                }
-
-                if (isalive2)
-                {
-                    Console.SetCursorPosition(obj2x, obj2y);
-                    Console.Write("2");
-                }
-
-                if (isalive3)
-                {
-                    Console.SetCursorPosition(obj3x, obj3y);
-                    Console.Write("3");
-                }
+                renderer.Update();
             }
         }
     }
+
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public void Randomise()
+        {
+            Random random = new Random();
+            
+            X += random.Next(-1, 1);
+            Y += random.Next(-1, 1);
+
+            if (X < 0) X = 0;
+            if (Y < 0) Y = 0;
+        }
+    }
+
+    public class Object
+    {
+        public Object(Position position)
+        {
+            Position = position;
+        }
+        
+        public Position Position { get; }
+
+        public void RandomisePosition() => 
+            Position.Randomise();
+    }
+
+    public class Renderer
+    {
+        private readonly Object[] _objects;
+        private readonly bool[] _isObjectAlive;
+        
+        public Renderer(Object[] objects)
+        {
+            _objects = objects;
+            _isObjectAlive = new bool[objects.Length];
+        }
+
+        public void Update()
+        {
+            for (int i = 0; i < _objects.Length; i++)
+            {
+                if (i < _objects.Length - 1)
+                {
+                    for (int j = i; j < _objects.Length - 1; j++)
+                    {
+                        bool isAlive = AreObjectsPositionsEqual(_objects[j], _objects[j + 1]);
+                        _isObjectAlive[j] = isAlive;
+                        _isObjectAlive[j + 1] = isAlive;
+                    }
+                }
+
+                _objects[i].RandomisePosition();
+                
+                if (_isObjectAlive[i]) 
+                    DrawObject(_objects[i], i);
+            }
+        }
+
+        private bool AreObjectsPositionsEqual(Object obj1, Object obj2) => 
+            obj1.Position.X == obj2.Position.X && obj1.Position.Y == obj2.Position.Y;
+
+        private void DrawObject(Object obj, int i)
+        {
+            Console.SetCursorPosition(obj.Position.X, obj.Position.Y);
+            Console.Write(i);
+        }
+          for (int j = i; j < objects.Length - 1; j++)
+          {
+            bool isAlive = objects[j].Position != objects[j + 1].Position;
+            isObjectAlive[j] = isAlive;
+            isObjectAlive[j + 1] = isAlive;
+          }
+        }
+
+        objects[i].Move();
+
+        if (isObjectAlive[i])
+          Renderer.DrawAt(objects[i].Position, i);
+      }
+    }
+  }
+
+  public class Position
+  {
+    public Position(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public static bool operator ==(Position position1, Position position2)
+      => position1.X == position2.X && position1.Y == position2.Y;
+
+    public static bool operator !=(Position position1, Position position2)
+      => !(position1 == position2);
+
+    public static Position operator +(Position position1, Position position2)
+    {
+      int x = position1.X + position2.X;
+      int y = position1.Y + position2.Y;
+      
+      if (x < 0) x = 0;
+      if (y < 0) y = 0;
+      
+      return new Position(x, y);
+    }
+  }
+
+  public class Object
+  {
+    public Object(Position position)
+    {
+      Position = position;
+    }
+
+    public Position Position { get; private set; }
+
+    public void Move() => 
+      Position += RandomPosition();
+
+    private static Position RandomPosition()
+    {
+      Random random = new Random();
+      return new Position(random.Next(-1, 1), random.Next(-1, 1));
+    }
+  }
+
+  public class Renderer
+  {
+    public static void DrawAt(Position position, int i)
+    {
+      Console.SetCursorPosition(position.X, position.Y);
+      Console.Write(i);
+    }
+  }
 }
